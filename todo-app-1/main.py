9f634e874d5563804203382f161a0a1968fd5a74
Sabith <masterdevsabith@gmail.com>
2025-08-08 22:08:27 +0530

    did many changes

--- todo-app-1/main.py
+++ todo-app-1/main.py
@@ -5,30 +5,30 @@ tasks = []
 
 
 #### FUNCTIONS####
-def add_a_task(id):
-    task_to_add = input("Enter your task : ").strip()
+def add_a_task():
+    task_to_add = input("Enter your task : ").strip().lower()
     status = 'PENDING'
-    tasks.append({"id": id+1, "task": task_to_add, "status": status})
+    tasks.append({"task": task_to_add, "status": status})
 
 
 def view_tasks():
-    for task in tasks:
-        print(f'{task["id"]} | {task["task"]} | {task["status"]}')
+    for index, task in enumerate(tasks, start=1):
+        print(f'{index} | {task["task"]} - {task["status"]}')
 
 
 def mark_task_as_completed(id):
-    for task in tasks:
-        if task["id"] == id:
-            task["status"] = "COMPLETED"
+    for index, task in enumerate(tasks, start=0):
+        if index == id:
+            tasks[index]['status'] = 'DONE'
 
 
 def delete_a_task(id):
-    for task in tasks:
-        if task["id"] == id:
-            tasks.remove(task)
+    for index, task in enumerate(tasks, start=0):
+        if index == id:
+            tasks.remove(tasks[index])
 
 
-#### REAL FUNCTIONALITY####
+        #### REAL FUNCTIONALITY####
 while True:
     print('''
 ----------------
@@ -40,24 +40,26 @@ while True:
 ----------------
 ''')
 
+    print(tasks)
     user_choice = input("enter your choice: ").strip()
     if not user_choice.isnumeric():
         print("enter a numeric value")
-        sys.exit()
 
     user_choice = int(user_choice)
     if user_choice not in (1, 2, 3, 4, 5):
         print("Choose option from 1-5")
 
     if user_choice == 1:
-        add_a_task(len(tasks))
+        add_a_task()
     if user_choice == 2:
         view_tasks()
     if user_choice == 3:
-        update_this_id = int(input("which task (enter the id) : ").strip())
-        mark_task_as_completed(update_this_id)
+        update_this_id = int(
+            input("which task DONE ? (enter the id) : ").strip())
+        mark_task_as_completed(update_this_id-1)
     if user_choice == 4:
-        update_this_id = int(input("which task (enter the id) : ").strip())
-        delete_a_task(update_this_id)
+        delete_this_id = int(
+            input("which task to delete ? (enter the id) : ").strip())
+        delete_a_task(delete_this_id-1)
     if user_choice == 5:
         sys.exit()
