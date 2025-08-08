0c37f2b540f7f8f761bc66f6529e51cc9fe5a25c
Sabith <masterdevsabith@gmail.com>
2025-08-08 20:16:15 +0530

    some updates

--- rock-paper-scissors/main.py
+++ rock-paper-scissors/main.py
@@ -4,13 +4,14 @@ import random
 number_of_rounds = input(
     "number of rounds needed (only odd number of rounds) :")
 iteration = 0
-if int(number_of_rounds) % 2 == 0:
+if number_of_rounds.isnumeric() and int(number_of_rounds) % 2 == 0:
     print("not odd")
 
 
 items = ['rock', 'paper', 'scissors']
 human_score = 0
 system_score = 0
+winner = ""
 
 
 def picking_an_item():
@@ -23,7 +24,8 @@ def picking_an_item():
     if user_input not in ('rock', 'paper', 'scissors'):
         iteration -= 1
         print("CHOOSE ANYTHING B/W ROCK | PAPER | SCISSORS")
-
+    elif user_input == system_input:
+        iteration -= 1
     elif user_input == 'rock' and system_input == 'paper':
         system_score += 1
     elif user_input == 'paper' and system_input == 'scissors':
@@ -38,16 +40,31 @@ def picking_an_item():
     elif user_input == 'scissors' and system_input == 'paper':
         human_score += 1
 
-    print(f'your choice : {user_input} | system input : {system_input}')
-    print(f'your score : {human_score} | system score : {system_score}')
+    print(f'you : {user_input} | system  : {system_input}')
 
 
-if int(number_of_rounds) >= 1 and number_of_rounds.isnumeric() and int(number_of_rounds) % 2 != 0:
+if number_of_rounds.isnumeric() and int(number_of_rounds) >= 1 and int(number_of_rounds) % 2 != 0:
+
     while iteration < int(number_of_rounds):
         picking_an_item()
         iteration += 1
 
+    if human_score > system_score:
+        winner = 'You WON the game'
+    elif human_score < system_score:
+        winner = 'System WON the game'
+    else:
+        winner = "TIE TIE"
+
     print(f'''
             YOUR SCORE : {human_score}
             SYSTEM SCORE : {system_score}
+            WINNER : {winner}
 ''')
+
+    # do_you_want_to_play_again = input(
+    #     "do you want to play again ? : (Yes/No)").strip().lower()
+    # if do_you_want_to_play_again == 'yes':
+    #     iteration = 0
+    # else:
+    #     pass
