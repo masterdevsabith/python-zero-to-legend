40d9c61475bb88a0287e889aa185d6f13e65cf9c
Sabith <masterdevsabith@gmail.com>
2025-08-08 19:59:33 +0530

    working well

--- rock-paper-scissors/main.py
+++ rock-paper-scissors/main.py
@@ -1,14 +1,53 @@
-human_score = 0
-system_score = 0
+import random
 
-items = ['rock', 'paper', 'scissors']
 
 number_of_rounds = input(
     "number of rounds needed (only odd number of rounds) :")
-
+iteration = 0
 if int(number_of_rounds) % 2 == 0:
     print("not odd")
 
 
-while int(number_of_rounds) >= 1 and number_of_rounds.isnumeric() and int(number_of_rounds) % 2 != 0:
-    print("HI")
+items = ['rock', 'paper', 'scissors']
+human_score = 0
+system_score = 0
+
+
+def picking_an_item():
+    global human_score
+    global system_score
+    global iteration
+    user_input = input("your choose (ROCK/PAPER/SCISSORS) : ").strip().lower()
+    system_input = random.choice(items)
+
+    if user_input not in ('rock', 'paper', 'scissors'):
+        iteration -= 1
+        print("CHOOSE ANYTHING B/W ROCK | PAPER | SCISSORS")
+
+    elif user_input == 'rock' and system_input == 'paper':
+        system_score += 1
+    elif user_input == 'paper' and system_input == 'scissors':
+        system_score += 1
+    elif user_input == 'scissors' and system_input == 'rock':
+        system_score += 1
+
+    elif user_input == 'rock' and system_input == 'scissors':
+        human_score += 1
+    elif user_input == 'paper' and system_input == 'rock':
+        human_score += 1
+    elif user_input == 'scissors' and system_input == 'paper':
+        human_score += 1
+
+    print(f'your choice : {user_input} | system input : {system_input}')
+    print(f'your score : {human_score} | system score : {system_score}')
+
+
+if int(number_of_rounds) >= 1 and number_of_rounds.isnumeric() and int(number_of_rounds) % 2 != 0:
+    while iteration < int(number_of_rounds):
+        picking_an_item()
+        iteration += 1
+
+    print(f'''
+            YOUR SCORE : {human_score}
+            SYSTEM SCORE : {system_score}
+''')
