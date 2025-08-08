6c2862d7d3316bfbfb679cdcea76f4f264ba8ccd
Sabith <masterdevsabith@gmail.com>
2025-08-08 16:48:39 +0530

    did a try except block

--- number-guessing-game/main.py
+++ number-guessing-game/main.py
@@ -1,4 +1,5 @@
 import random
+import math
 
 max_number = 20
 min_number = 1
@@ -7,25 +8,27 @@ total_guesses = 3
 
 
 while total_guesses > 0:
-    user_input = int(
-        input(f"guess the number (you have {total_guesses} chance left ): ").strip())
-    total_guesses -= 1
+    try:
+        user_input = int(
+            input(f"guess the number (you have {total_guesses} chance left ): ").strip())
+        total_guesses -= 1
 
-    if user_input > max_number or user_input < min_number:
-        print(f'Enter a number between {min_number} and {max_number}')
-    elif user_input == system_input:
-        print('correct')
-        break
-    elif abs(user_input-system_input) <= 5:
-        print('close')
+        if user_input > max_number or user_input < min_number:
+            print(f'Enter a number between {min_number} and {max_number}')
+            total_guesses += 1
+        elif user_input == system_input:
+            print('correct')
+            break
+        elif abs(user_input-system_input) <= 5:
+            print('close')
 
-    elif user_input > system_input:
-        print("too high")
-    elif user_input < system_input:
-        print('too low')
+        elif user_input > system_input:
+            print("too high")
+        elif user_input < system_input:
+            print('too low')
+    except ValueError:
+        print("enter a number NOT A STRING")
 
 
 if total_guesses == 0 and user_input != system_input:
     print('out of guesses')
-elif total_guesses == 0:
-    print("try again shithead")
