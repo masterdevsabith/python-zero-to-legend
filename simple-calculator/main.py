f394f4226da065af1dbad24abf757bc7e94c1a9a
Sabith <masterdevsabith@gmail.com>
2025-08-10 16:12:20 +0530

    simple calculator done

--- simple-calculator/main.py
+++ simple-calculator/main.py
@@ -1,9 +1,47 @@
-import tkinter as tk
-
-root = tk.Tk()
-root.title("Pack Example")
+import sys
 
 
-root.geometry("600x400")
+def add_two_numbers(a, b):
+    return a+b
 
-root.mainloop()
+
+def subtract_two_numbers(a, b):
+    return a-b
+
+
+def multiply_two_numbers(a, b):
+    return a*b
+
+
+def divide_two_numbers(a, b):
+    return a/b
+
+
+while True:
+    first_number = input("enter first number: ")
+    second_number = input("enter second number: ")
+    operation_needed = input("enter operation needed (+, -, *, /): ")
+
+    if not first_number.isnumeric():
+        print("please enter a number")
+        sys.exit()
+
+    if not second_number.isnumeric():
+        print("please enter a number")
+        sys.exit()
+
+    if operation_needed not in ('+', '-', '*', '/'):
+        print("please enter a valid operation !!!")
+        sys.exit()
+
+    first_number = int(first_number)
+    second_number = int(second_number)
+
+    if operation_needed == '+':
+        print(add_two_numbers(first_number, second_number))
+    elif operation_needed == '-':
+        print(subtract_two_numbers(first_number, second_number))
+    elif operation_needed == '*':
+        print(multiply_two_numbers(first_number, second_number))
+    elif operation_needed == '/':
+        print(divide_two_numbers(first_number, second_number))
